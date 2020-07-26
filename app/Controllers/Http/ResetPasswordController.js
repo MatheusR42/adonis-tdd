47ba587d0d15a80073fbec0861b71e3956c5a0e8
matheusr42 <matheus.araujo.r42@gmail.com>
2020-07-26 11:13:02 -0300

    feat: expire password reset token

--- app/Controllers/Http/ResetPasswordController.js
+++ app/Controllers/Http/ResetPasswordController.js
@@ -1,12 +1,18 @@
 "use strict";
+const { isBefore, parseISO, subHours } = require('date-fns')
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Token = use("App/Models/Token");
 
 class ResetPasswordController {
-  async store({ request }) {
+  async store({ request, response }) {
     const { token, password } = request.only(["token", "password"]);
 
     const userToken = await Token.findBy("token", token);
+
+    if (isBefore(parseISO(userToken.created_at), subHours(new Date(), 2))) {
+      return response.status(400).json({ error: 'The reset password token has expired'})
+    }
+
     const user = await userToken.user().fetch();
 
     user.password = password;
--- test/functional/forgotpassword.spec.js
+++ test/functional/forgotpassword.spec.js
@@ -1,13 +1,11 @@
 const { test, trait } = use("Test/Suite")("Session");
-
+const { subHours, format } = require("date-fns");
 const Mail = use("Mail");
 const Hash = use("Hash");
+const Database = use("Database");
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use("Factory");
-
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const User = use("App/Models/User");
 
 trait("Test/ApiClient");
 trait("DatabaseTransactions");
@@ -37,7 +35,7 @@ test("it should be able to reset password", async ({ assert, client }) => {
   const email = "matheus@gmail.com";
   const user = await Factory.model("App/Models/User").create({ email });
   const userToken = await Factory.model("App/Models/Token").make({
-    type: 'forgotpassword'
+    type: "forgotpassword",
   });
 
   await user.tokens().save(userToken);
@@ -53,7 +51,38 @@ test("it should be able to reset password", async ({ assert, client }) => {
 
   response.assertStatus(204);
 
-  await user.reload()
+  await user.reload();
   const checkPassword = await Hash.verify("123456", user.password);
   assert.isTrue(checkPassword);
+});
+
+test("it cannot reset password after 2h of request", async ({
+  assert,
+  client,
+}) => {
+  const email = "matheus@gmail.com";
+  const user = await Factory.model("App/Models/User").create({ email });
+  const userToken = await Factory.model("App/Models/Token").make({
+    type: "forgotpassword",
+  });
+
+  await user.tokens().save(userToken);
+
+  const dateWithSub = format(subHours(new Date(), 3), "yyyy-MM-dd HH:ii:ss");
+  await Database.table("tokens")
+    .where("token", userToken.token)
+    .update("created_at", dateWithSub);
+
+  await userToken.reload();
+
+  const response = await client
+    .post("/reset")
+    .send({
+      token: userToken.token,
+      password: "123456",
+      password_confirmation: "123456",
+    })
+    .end();
+
+  response.assertStatus(400)
 });
