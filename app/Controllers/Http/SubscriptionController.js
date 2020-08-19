f1629f95ef91531d65e71163e5fadb11e3b5e401
MatheusR42 <matheus.araujo.r42@gmail.com>
2020-08-18 23:56:41 -0300

    limit workshop subscription by section

--- app/Controllers/Http/SubscriptionController.js
+++ app/Controllers/Http/SubscriptionController.js
@@ -1,9 +1,26 @@
 'use strict';
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Workshop = use('App/Models/Workshop');
 
 class SubscriptionController {
   async store({ response, params, auth }) {
     const user = await auth.getUser();
     const { workshop_id } = params;
+
+    const workshop = await Workshop.find(workshop_id);
+    const checkSubscription = await user
+      .subscriptions()
+      .where('section', workshop.section)
+      .first();
+
+    if (checkSubscription) {
+      return response
+        .status(400)
+        .json({
+          error:
+            'You cannot subscribe to more than one workshop in the same section',
+        });
+    }
 
     await user.subscriptions().attach(workshop_id);
 
@@ -11,10 +28,10 @@ class SubscriptionController {
   }
 
   async destroy({ params, auth }) {
-    const user = await auth.getUser()
-    const { workshop_id } = params
+    const user = await auth.getUser();
+    const { workshop_id } = params;
 
-    await user.subscriptions().detach(workshop_id)
+    await user.subscriptions().detach(workshop_id);
   }
 }
 
--- test/functional/subscription.spec.js
+++ test/functional/subscription.spec.js
@@ -28,6 +28,33 @@ test('it should be able to subscribe to a workshop', async ({
   assert.equal(subscriptionWorkshop.id, workshop.id);
 });
 
+
+test('it should not be able to subscribe to multiple workshops at same section', async ({
+  assert,
+  client,
+}) => {
+  const user = await Factory.model('App/Models/User').create();
+  const workshop1 = await Factory.model('App/Models/Workshop').create({
+    section: 1
+  });
+  const workshop2 = await Factory.model('App/Models/Workshop').create({
+    section: 1
+  });
+
+  await user.subscriptions().attach(workshop1.id);
+
+  const response = await client
+    .post(`/workshops/${workshop2.id}/subscriptions`)
+    .loginVia(user, 'jwt')
+    .end();
+
+  response.assertStatus(400);
+
+  const subscriptions = await user.subscriptions().count();
+
+  assert.equal(subscriptions[0]['count(*)'], 1);
+});
+
 test('it should be able to unsubscribe a workshop', async ({
   assert,
   client,
